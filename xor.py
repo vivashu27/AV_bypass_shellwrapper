5639c0b5e5a91a0503a5adc6226283833b8adb3b
luffy27 <52876924+vivashu27@users.noreply.github.com>
2023-07-16 18:27:14 +0530

    Update xor.py

--- xor.py
+++ xor.py
@@ -1,30 +1,33 @@
-#!/usr/bin/python3.11
+#!/usr/bin/python3.10
 
 import sys
 
-key="#12122$14112"
-j=0
+key = b"#12122$14112"
+j = 0
 
-def xor(plain: str):
-	xored=""
-	j=0
-	for i in plain:
-		xored+=str(i^ord(key[j%len(key)]))
-		j+=1
-	return xored	
+def xor(plain: bytes):
+    xored = b""
+    j = 0
+    for i in plain:
+        xored += bytes([i ^ key[j % len(key)]])
+        j += 1
+    return xored
 
-def shell_code(xored: str):
-	shell=''
-	for i in xored:
-		shell+="\\x"+hex(ord(i)).replace("0x","")
-	print(shell)
+def shell_code(xored: bytes):
+    shell = ""
+    for i in xored:
+        shell += "\\x" + hex(i)[2:].zfill(2)
+    print(shell)
 
 def main():
-	s=sys.argv[1]
-	f=open(s,"rb")
-	plain=f.read()
-	xored=str(xor(plain))
-	shell_code(xored)
+    if len(sys.argv) < 2:
+        print("Usage: python encrypt.py <filename>")
+        return
 
+    filename = sys.argv[1]
+    with open(filename, "rb") as f:
+        plain = f.read()
+        xored = xor(plain)
+        shell_code(xored)
 
 main()
