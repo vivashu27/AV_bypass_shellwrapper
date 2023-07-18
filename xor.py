a9948a6f12978619ca65c102b8e923e7b6bb8499
luffy27 <52876924+vivashu27@users.noreply.github.com>
2023-07-18 19:01:34 +0530

    Update xor.py

--- xor.py
+++ xor.py
@@ -2,32 +2,23 @@
 
 import sys
 
-key = b"#12122$14112"
-j = 0
+def encrypt_payload(input_file, key):
+    with open(input_file, 'rb') as f:
+        payload = f.read()
 
-def xor(plain: bytes):
-    xored = b""
-    j = 0
-    for i in plain:
-        xored += bytes([i ^ key[j % len(key)]])
-        j += 1
-    return xored
+    encrypted_payload = bytearray(payload)
+    key_length = len(key)
 
-def shell_code(xored: bytes):
-    shell = ""
-    for i in xored:
-        shell += "\\x" + hex(i)[2:].zfill(2)
-    print(shell)
+    for i in range(len(encrypted_payload)):
+        encrypted_payload[i] ^= ord(key[i % key_length])
+
+    encrypted_payload_hex = ''.join([rf'\x{byte:02x}' for byte in encrypted_payload])
+    print(encrypted_payload_hex)
+
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python encrypt.py <filename>")
-        return
-
-    filename = sys.argv[1]
-    with open(filename, "rb") as f:
-        plain = f.read()
-        xored = xor(plain)
-        shell_code(xored)
+    input_file = sys.argv[1]   
+    key = "#12122$14112"    
+    encrypt_payload(input_file, key)
 
 main()
